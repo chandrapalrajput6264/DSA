01a2c794066b72eb5c391f1db20807c7893440cc
chandrapal Rajput <chandrapalrajput6264@gmail.com>
2026-09-21 20:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (24.30%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int i = 0;
+        int j = 0;
+
+        int n = version1.length();
+        int m = version2.length();
+
+        while (i < n || j < m) {
+            long num1 = 0;
+            long num2 = 0;
+
+            // Read revision from version1
+            while (i < n && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+
+            // Read revision from version2
+            while (j < m && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+
+            if (num1 < num2) {
+                return -1;
+            }
+
+            if (num1 > num2) {
+                return 1;
+            }
+
+            // Skip '.'
+            if (i < n) {
+                i++;
+            }
+
+            if (j < m) {
+                j++;
+            }
+        }
+
+        return 0;
+    }
+}
